a34ca747211a96ebf0db4e2f60294770f10a1456
HrvojeZec <98240642+HrvojeZec@users.noreply.github.com>
2024-07-01 12:38:31 +0200

    Delete Server/dist directory

--- Server/dist/constants/httpResponseStauts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.httpStatus = void 0;
-exports.httpStatus = {
-    BAD_REQUEST: 400,
-    NOT_FOUND: 404,
-    INTERNAL_SERVER_ERROR: 500,
-    MULTI_STATUS: 207,
-};
