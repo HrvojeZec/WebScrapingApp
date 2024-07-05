aec707c1f3d63f5a82b5e52914a1cbfd05d81330
Hrvoje Zec <hrvoje.zec99@gmail.com>
2024-07-05 17:16:31 +0200

    Setup Pettier and ESLint

--- Client/eslint.config.js
+++ Client/eslint.config.js
@@ -1,24 +1,35 @@
 // eslint.config.js
 
-import { ESLint } from "@eslint/js";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import { ESLint } from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
     languageOptions: {
       parser: tsParser,
       ecmaVersion: 2020,
-      sourceType: "module",
+      sourceType: 'module',
       globals: {
         ...globals.browser,
         ...globals.node,
       },
     },
     plugins: {
-      "@typescript-eslint": tseslint,
+      '@typescript-eslint': tseslint,
+      prettier: {
+        version: ESLint.latestVersion,
+        rules: {
+          'prettier/prettier': 'error',
+        },
+      },
     },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ],
     rules: {
       ...eslint.configs.recommended.rules,
       ...tseslint.configs.recommended.rules,
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,14 +1,12 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import React from 'react';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-   <React.StrictMode>
-    <App />
-   </React.StrictMode>
-  </BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
 );
-
-
