c761c78d36dacda1dd2493730be7228a6f5d6bd6
Hrvoje Zec <hrvoje.zec99@gmail.com>
2024-10-25 15:46:39 +0200

    Implement test for findAllProducts and findRandomProducts functions

--- Server/__tests__/product-service.test.ts
+++ Server/__tests__/product-service.test.ts
@@ -1,7 +1,10 @@
 import {
-  findProductsByKeyword,
-  paginationHandler,
   findAllKeywords,
+  findProductsByKeyword,
+  findAllProducts,
+  findRandomProducts,
+  findProductsByScrapeId,
+  paginationHandler,
 } from '../src/router/product/product-service';
 import {
   connectMemoryMongoDB,
@@ -11,6 +14,7 @@ import {
 import { Product } from '../src/model/productModel';
 import { testStoreData, testProductData } from './test-data/productData';
 import { Stores } from '../src/model/storesModel';
+import { describe } from 'node:test';
 
 beforeAll(async () => {
   await connectMemoryMongoDB();
@@ -28,13 +32,16 @@ afterEach(async () => {
 afterAll(async () => {
   await disconnectMemoryMongoDB();
 });
+
 describe('findAllkeywords function', () => {
   test('should find all keyword', async () => {
     const keywords = await findAllKeywords();
+
     expect(keywords).toContain('iphone');
     expect(keywords).toContain('laptop');
   });
 });
+
 describe('findProductsByKeyword function', () => {
   test('should return products with store attributes by keyword', async () => {
     const { skip, limit } = await paginationHandler('iphone', 1, 10);
@@ -44,5 +51,24 @@ describe('findProductsByKeyword function', () => {
     expect(products[0].storeName).toBe('Test Mall');
     expect(products[0]).toHaveProperty('logo');
     expect(products[0].keyword).toBe('iphone');
+  });
+});
+
+describe('findAllProducts function', () => {
+  test('should find all Products', async () => {
+    const products = await findAllProducts();
+
+    expect(products.length).toBeGreaterThan(0);
+    expect(products[0].storeName).toBe('Test Mall');
+    expect(products[0]).toHaveProperty('logo');
+    expect(products[0].keyword).toBe('iphone');
+  });
+});
+
+describe('findRandomProducts function', () => {
+  test('should find 10 random products', async () => {
+    const products = await findRandomProducts();
+
+    expect(products.length).toBe(10);
   });
 });
